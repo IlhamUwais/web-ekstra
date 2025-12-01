7fddfd79a88585633694bd2dff78c9448e7e9df2
Ilham <ilhamsukamesi@gmail.com>
2025-12-01 14:44:49 +0700

    Update db.js ke TiDB Cloud

--- app/db/db.js
+++ app/db/db.js
@@ -1,13 +1,21 @@
-import mysql from 'mysql2/promise';  
+import mysql from 'mysql2';
 
-const db = mysql.createPool({  
-  host: 'localhost',  
-  user: 'root',  
-  password: '',  
-  database: 'presensi_ekstra',  
-  waitForConnections: true,  
-  connectionLimit: 10,  
-  queueLimit: 0  
-});  
+// Kita gunakan createPool (bukan createConnection)
+// Agar koneksi tidak putus-nyambung saat banyak yang akses
+const db = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: 4000, // Port standar TiDB
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    // Settingan SSL (WAJIB untuk TiDB Cloud)
+    ssl: {
+        minVersion: 'TLSv1.2',
+        rejectUnauthorized: true
+    }
+});
 
-export default db;  
+export default db;
