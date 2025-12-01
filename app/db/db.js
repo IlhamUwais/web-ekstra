1a9bc4870cc251537a4c81fdf56886c2693cccb8
Ilham <ilhamsukamesi@gmail.com>
2025-12-01 14:51:27 +0700

    Ganti mysql2 jadi promise

--- app/db/db.js
+++ app/db/db.js
@@ -1,17 +1,15 @@
-import mysql from 'mysql2';
+// PENTING: Gunakan 'mysql2/promise'
+import mysql from 'mysql2/promise';
 
-// Kita gunakan createPool (bukan createConnection)
-// Agar koneksi tidak putus-nyambung saat banyak yang akses
 const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: 4000, // Port standar TiDB
+    port: 4000,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
-    // Settingan SSL (WAJIB untuk TiDB Cloud)
     ssl: {
         minVersion: 'TLSv1.2',
         rejectUnauthorized: true
